90d43af51cd534ced47f1c0a255159e1d06b697c
dani <mayor@dani.town>
2019-11-16 16:34:44 -0500

    init

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+const app = express()
+const port = 3001 || process.env.PORT
+const fetch = require('node-fetch')
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.get('/twitter-embed-code', async (req, res) => {
+
+  let response = await fetch(`https://publish.twitter.com/oembed?url=https://twitter.com/${req.query.name}/status/${req.query.status}`)
+  let json = await response.json()
+
+  res.send(json.html)
+
+})
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
