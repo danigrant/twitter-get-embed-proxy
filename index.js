2117b6ab47c79cec5a90ee6a88b74a22ada18379
dani <mayor@dani.town>
2019-11-16 16:48:15 -0500

    init

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const express = require('express')
+var cors = require('cors')
 const app = express()
 const port = process.env.PORT || 80
 const fetch = require('node-fetch')
+
+app.use(cors())
 
 app.get('/', (req, res) => res.send('Hello World!'))
 
